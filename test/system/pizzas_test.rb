08b369175e3e622bdc6025e18a017f67317c3878
Travis Frazier <travisfrazier.me@gmail.com>
2017-11-18 17:37:01 +0000

    fist commit

---
+++ test/system/pizzas_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class PizzasTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit pizzas_url
+  #
+  #   assert_selector "h1", text: "Pizza"
+  # end
+end
